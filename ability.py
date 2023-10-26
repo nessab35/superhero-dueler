1a87baf01fcdfe990a46306878cb14293d4e0118
nessab35 <nessa.begay@students.dominican.edu>
2023-10-25 19:45:22 -0700

    created armor

---
+++ ability.py
@@ -0,0 +1,19 @@
+# ability.py
+import random
+
+class Ability:
+    def __init__(self, name, max_damage):
+        self.name = name
+        self.max_damage = max_damage
+
+    def attack(self):
+        '''Return a value between 0 and the value set by self.max_damage'''
+        random_value = random.randint(0, self.max_damage)
+        return random_value
+
+
+# Testing
+if __name__ == "__main__":
+    ability = Ability("debugging ability", 20)
+    print(ability.name)
+    print(ability.attack())
---
+++ armor.py
@@ -0,0 +1,17 @@
+# armor.py
+import random
+class Armor:
+    def __init__(self, name, max_block):
+        self.name = name
+        self.max_block = max_block
+
+    def block(self):
+        random_value = random.randint(0, self.max_block)
+        return random_value
+
+
+# testing
+if __name__ == "__main__":
+    armor = Armor("debugging shield", 10)
+    print(armor.name)
+    print(armor.block())
